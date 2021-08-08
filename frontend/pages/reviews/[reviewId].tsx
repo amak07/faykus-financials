a279312c74cc4350db090d35d7edd5a258563b68
Abel Mak <abelmak07@gmail.com>
2021-08-07 19:23:49 -0500

    test

--- frontend/pages/reviews/[reviewId].tsx
+++ frontend/pages/reviews/[reviewId].tsx
@@ -3,18 +3,6 @@ import React from "react";
 // import useFetch from "../hooks/useFetch";
 
 export default function ReviewDetails() {
-  // const { id } = useParams<{ id: string }>();
-  // const {
-  //   loading,
-  //   error,
-  //   data: review,
-  // } = useFetch<{ rating: number; title: string; body: string }>(
-  //   "http://localhost:1337/reviews/" + id
-  // );
-
-  // if (loading) return <p>Loading...</p>;
-  // if (error) return <p>Error :(</p>;
-
   return (
     <div className="review-card">
       Review Details
