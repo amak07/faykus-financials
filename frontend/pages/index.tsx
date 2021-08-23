cc6ffc68b4c7322a186a60a173ed757a8ca890c5
Abel Mak <abelmak07@gmail.com>
2021-08-23 18:20:22 -0500

    home page head added

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,22 @@
+import Head from "next/head";
+
 function HomePage() {
-  return <h1>The Home Page</h1>;
+  return (
+    <>
+      <Head>
+        <title>Faykus Financial, LLC</title>
+        <meta
+          name="description"
+          content="Wealth management firm based in Austin, TX"
+        />
+        <meta property="og:site_name" content="Faykus Financial, LLC" />
+        <meta property="og:url" content="https://faykusfinancial.com/" />
+        <meta property="og:title" content="Home" />
+        <meta property="og:type" content="article" />
+      </Head>
+      <h1>The Home Page</h1>
+    </>
+  );
 }
 
 export default HomePage;
